2f7785430bf694ab04b6aef0e2cc67617e2d6288
Zhang Chi <zliming@outlook.com>
2021-04-29 18:13:07 +0800

    :construction: SolutionTree component

---
+++ src/MarauderMap.Blazor/Components/SolutionTrees/SolutionTree.razor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace MarauderMap.Blazor.Components.SolutionTrees
+{
+    public partial class SolutionTree
+    {
+        private Lazy<Task<IJSObjectReference>> _jsTask;
+
+        [Inject]
+        private IJSRuntime JsRuntime { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            _jsTask = _jsTask = new Lazy<Task<IJSObjectReference>>(() => JsRuntime.InvokeAsync<IJSObjectReference>(
+                "import", "./solutionTree.js").AsTask());
+        }
+
+        private async Task OnToggleClickedAsync(string id)
+        {
+            var js = await _jsTask.Value;
+            await js.InvokeVoidAsync("toggle", id);
+        }
+    }
+}
---
+++ src/MarauderMap.Blazor/wwwroot/solutionTree.js
@@ -0,0 +1,7 @@
+﻿
+export function toggle(id) {
+    let collapse = $('#' + id).next();
+    if (collapse && collapse.attr('class').includes('collaps')) {
+        collapse.collapse('toggle');
+    }
+}
